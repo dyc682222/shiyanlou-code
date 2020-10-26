25bafa73d7a11e4e9bac9c99608d3db8be989780
dyc682222 <daiyc6@hotmail.com>
2020-10-26 20:39:31 +0800

    seven 2

--- jump7.py
+++ jump7.py
@@ -1,7 +1,9 @@
-i=0
+i=0#count i
 while i<=100:
     i=i+1
-    if i%7==0 or i%10==7 or i//10==7:
+    if i%7==0 or i%10==7 or i//10==7:   #seven
         continue
     else:
         print(i)
+
+#this is a program.
